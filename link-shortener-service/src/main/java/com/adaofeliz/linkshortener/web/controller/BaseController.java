e123cea99b2a2ca1d14dd3d5c5da36d96a487a5f
adaofeliz <pinto@clipkit.de>
2014-12-03 03:47:16 +0000

    Changing BaseController implementing the index.html and the short link redirect

--- link-shortener-service/src/main/java/com/adaofeliz/linkshortener/web/controller/BaseController.java
+++ link-shortener-service/src/main/java/com/adaofeliz/linkshortener/web/controller/BaseController.java
@@ -1,16 +1,33 @@
 package com.adaofeliz.linkshortener.web.controller;
 
+import com.adaofeliz.linkshortener.service.ShortLinkService;
+import com.adaofeliz.linkshortener.service.domain.ShortLink;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class BaseController {
 
+    @Autowired
+    private ShortLinkService shortLinkService;
+
     @ResponseBody
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String helloWorld() {
-        return "Hello World: Link Shortener Service.";
+    public void applicationIndex(HttpServletResponse httpServletResponse) throws IOException {
+        httpServletResponse.sendRedirect("/swagger/index.html");
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/{shortUri}", method = RequestMethod.GET)
+    public void getLink(@PathVariable String shortUri, HttpServletResponse httpServletResponse) throws IOException {
+        ShortLink shortLink = shortLinkService.getShortLinkByShortUri(shortUri);
+        httpServletResponse.sendRedirect(shortLink.getOriginalUrl());
     }
 }
